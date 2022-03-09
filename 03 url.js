8404c7dcb439a76115863b4478fa024e0fdb07ce
Absar Ahmad <absarahmad.coder@gmail.com>
2022-03-09 13:58:36 +0500

    simple navigation through url on client request

--- 03 url.js
+++ 03 url.js
@@ -34,3 +34,24 @@
 // console.log(qdata.year); // 2022
 // console.log(qdata); // { year: '2022', month: 'february' }
 // console.log(q.query.month); // february
+
+
+//👀⏩ Simple Navigator 
+
+var http = require('http');
+var url = require('url');
+var fs = require('fs');
+http.createServer(function (req, res) {
+    var q = url.parse(req.url, true);
+    var filename = '.' + q.pathname;
+    fs.readFile(filename, function (err, data) {
+        if (err) {
+            res.writeHead(404, {'Content-Type': 'text/html'});
+            return res.end("404 Not Found");
+        }
+        res.writeHead(200,{'Content-Type':'text/html'});
+        res.write(data);
+        res.end();
+    });
+}).listen(8000);
+
