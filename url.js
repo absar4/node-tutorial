f84844811e10c01ba48ea7bea79b4fac5bcede80
Absar Ahmad <absarahmad.coder@gmail.com>
2022-03-09 13:08:43 +0500

    Adding more details to url section

--- url.js
+++ url.js
@@ -1,18 +1,18 @@
 //👀⏩ It holds the part of the url that comes after the domain name
-var http = require('http');
-http.createServer(function (req, res) {
-    res.writeHead(200, { 'Content-Type': 'text/html' });
-    res.write('url is = ' + req.url);
-    res.end(txt);
-}).listen(8000);
+// var http = require('http');
+// http.createServer(function (req, res) {
+//     res.writeHead(200, { 'Content-Type': 'text/html' });
+//     res.write('url is = ' + req.url);
+//     res.end(txt);
+// }).listen(8000);
 
 //👀⏩ There are built-in modules to easily split the query string into readable parts, such as the URL module.
-var http = require('http');
-var url = require('url');
-http.createServer(function (req, res) {
-    res.writeHead(200, { 'Content-Type': 'text/html' });
-    var qry = url.parse(req.url, true).query;
-    var txt = qry.year + " " + qry.month;
-    res.write('url is = ' + req.url);
-    res.end(txt);
-}).listen(8000)
+// var http = require('http');
+// var url = require('url');
+// http.createServer(function (req, res) {
+//     res.writeHead(200, { 'Content-Type': 'text/html' });
+//     var qry = url.parse(req.url, true).query;
+//     var txt = qry.year + " " + qry.month;
+//     res.write('url is = ' + req.url);
+//     res.end(txt);
+// }).listen(8000);
