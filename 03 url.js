20c3a81e90934514ee7b31e3f2b1d263f5cc169f
Absar Ahmad <absarahmad.coder@gmail.com>
2022-03-09 13:23:53 +0500

    minor file name chnaging for future ease

--- 03 url.js
+++ 03 url.js
@@ -16,3 +16,18 @@
 //     res.write('url is = ' + req.url);
 //     res.end(txt);
 // }).listen(8000);
+
+//👀⏩ url.parse() method will divide it into pieces and return a URL object with each part of the address as properties
+var url = require('url');
+var adr = 'http://localhost:8080/default.htm?year=2017&month=february';
+//----Parse the address:
+var q = url.parse(adr, true);
+
+/*The parse method returns an object containing url properties*/
+console.log(q.host);
+console.log(q.pathname);
+console.log(q.search);
+
+/*The query property returns an object with all the querystring parameters as properties:*/
+var qdata = q.query;
+console.log(qdata.month);
