24d8b3cef6e360fb8ab04cda99868072dac4fce6
Absar Ahmad <absarahmad.coder@gmail.com>
2022-03-11 17:19:13 +0500

    new file added

---
+++ 05 events.js
@@ -0,0 +1,15 @@
+// Every action on a computer is an event. Like when a connection is made or a file is opened.
+// Objects in Node.js can fire events
+var events = require('events');
+var eventEmitter = new events.EventEmitter();
+
+//Create an event handler:
+var myEventHandler = function () {
+    console.log('I hear a scream!');
+  }
+  
+  //Assign the event handler to an event:
+  eventEmitter.on('scream', myEventHandler);
+  
+  //Fire the 'scream' event:
+  eventEmitter.emit('scream');
