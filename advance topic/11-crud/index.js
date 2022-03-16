3648b6d9b18a068024d0044dbe126049095e1e56
Absar Ahmad <absarahmad.coder@gmail.com>
2022-03-16 23:03:44 +0500

    11 crud operation

---
+++ advance topic/11-crud/index.js
@@ -0,0 +1,29 @@
+const path = require('path')
+const fs = require('fs')
+
+// 👀⏩ Create File 
+// fs.writeFileSync('hahah.txt','this is lol')
+
+// 👀⏩ Read File 
+// fs.readFile('hahah.txt','utf8', (err, item) => {
+    //     if (!err) {
+        //         console.log(item);
+//     }
+// });
+
+// 👀⏩ Update File 
+// fs.appendFile('hahah.txt'," always smiling 😁",(err)=>{
+//     if (!err) {
+//         console.log('Updated Successfully');
+//     }
+// });
+
+// 👀⏩ Rename File
+// fs.rename('hahah.txt','haha.txt',(err)=>{
+//     if (!err) {
+//         console.log('file is updated successfully');
+//     }
+// });
+
+// 👀⏩ Deleting a file
+// fs.unlinkSync('haha.txt')
