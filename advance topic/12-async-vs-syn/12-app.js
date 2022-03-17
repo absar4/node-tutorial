84c6b8f196751cc0484c0cc9ea7cee924620d372
Absar Ahmad <absarahmad.coder@gmail.com>
2022-03-17 22:04:43 +0500

    12-async

---
+++ advance topic/12-async-vs-syn/12-app.js
@@ -0,0 +1,22 @@
+// Asynchronous vs Synchronous
+/**
+ * In synchronous operations tasks are performed one at a time and only when one is completed, the following is unblocked. In other words, you need to wait for a task to finish to move to the next one. In asynchronous operations, on the other hand, you can move to another task before the previous one finishes.
+ * 
+ * Node js is Asynchronous runtime environemnt because javascript is an Asynchronous programming language
+ */
+// 👀⏩  Asynchronous Example
+
+// console.log("start");
+// setTimeout(() => {
+//     console.log("process and logics");
+// }, 2000);
+// console.log("completed");
+
+let a = 10;
+let b = 0;
+
+setTimeout(() => {
+    b = 10;
+}, 2000);
+
+console.log(a + b);
