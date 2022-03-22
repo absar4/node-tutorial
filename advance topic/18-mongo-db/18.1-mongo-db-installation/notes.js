4e86776ce9fc8535ab5982cf08f8f9c0698e3a7c
Absar Ahmad <absarahmad.coder@gmail.com>
2022-03-22 22:48:53 +0500

    mongodb

---
+++ advance topic/18-mongo-db/18.1-mongo-db-installation/notes.js
@@ -0,0 +1,14 @@
+/**
+ * Mongodb is a database
+ * download mongodb (try > community) website
+ * install monodb
+ * mongo db compass tool
+ * mongo in cmd for checking
+ * check from ppt
+ * show dbs (cmd)
+ * use database_name (cmd)
+ * db.createCollection('collection_name')
+ * show collection
+ * db.collection_name.drop() then refresh db
+ * db.dropDatabase()
+ */
