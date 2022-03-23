8f26d7be0b51cd576b4d3bdf0b291ec2e6c539cf
absar ahmad <absarahmad.coder@gmail.com>
2022-03-23 11:07:46 +0500

    crud nts

--- advance topic/18-mongo-db/18.1-mongo-db-installation/18.2-crud-notes.js
+++ advance topic/18-mongo-db/18.1-mongo-db-installation/18.2-crud-notes.js
@@ -1,5 +1,8 @@
 /**
  * Mongodb CRUD
  * use db_name
+ * db.collection_name.insertOne({object object})
+ *  example:
+ *      db.products.insertOne({name:"iPhone",brand:"iPhone brand",category:"Smart Phones"})
  * 
  */
