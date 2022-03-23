86f8dc1c7d93a2b656a2a53c29a9a66881600906
absar ahmad <absarahmad.coder@gmail.com>
2022-03-23 11:11:27 +0500

    crud nts update

--- advance topic/18-mongo-db/18.1-mongo-db-installation/18.2-crud-notes.js
+++ advance topic/18-mongo-db/18.1-mongo-db-installation/18.2-crud-notes.js
@@ -4,5 +4,14 @@
  * db.collection_name.insertOne({object object})
  *  example:
  *      db.products.insertOne({name:"iPhone",brand:"iPhone brand",category:"Smart Phones"})
- * 
+ * collection_name > Add Data(btn) > insert Document 
+ * example:
+ *  {
+ * "id": {
+ *  ---------------
+ * },
+ * "name":"iPhone",
+ * "brand":"iPhone brand",
+ * "category":"Smart Phones"
+ * } 
  */
