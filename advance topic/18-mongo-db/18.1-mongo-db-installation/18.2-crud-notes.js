adb9cfbc4abe589806195d4d245cd40869043a1f
absar ahmad <absarahmad.coder@gmail.com>
2022-03-23 11:44:49 +0500

    crud nts

--- advance topic/18-mongo-db/18.1-mongo-db-installation/18.2-crud-notes.js
+++ advance topic/18-mongo-db/18.1-mongo-db-installation/18.2-crud-notes.js
@@ -4,7 +4,7 @@
  * db.collection_name.insertOne({object object})
  *  example:
  *      db.products.insertOne({name:"iPhone",brand:"iPhone brand",category:"Smart Phones"})
- * collection_name > Add Data(btn) > insert Document 
+ * collection_name > Add Data(btn) > insert Document
  * example:
  *  {
  * "id": {
@@ -13,5 +13,20 @@
  * "name":"iPhone",
  * "brand":"iPhone brand",
  * "category":"Smart Phones"
- * } 
+ * }
+ * _______________________________________________________
+ * Show Data:
+ * to show data in cmd write:
+ *  db.collection_name.find()
+ *  db.products.find()
+ * _______________________________________________________
+ * Update Data:
+ *  db.collection_name.updateOne({name:"iPhone"},{$set:{brand:"iPhone"}})
+ *  db.student.updateOne({name:"Bhannu"},{$set:{name:"Babita"}})
+ * _______________________________________________________
+ * Delete Data:
+ *  db.student.deleteOne({name:"Bhannu"})
+ *  db.student.deleteOne({age:17})
+ *  _______________________________________________________
+ * 
  */
