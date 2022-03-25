d280ce0d13c457ae78cd838921c5eed8b42c68f1
Absar Ahmad <absarahmad.coder@gmail.com>
2022-03-26 00:09:23 +0500

    where clause complete

--- advance topic/18-mysql/18.6-selecting-from-table-with-filter-in-nodejs.js
+++ advance topic/18-mysql/18.6-selecting-from-table-with-filter-in-nodejs.js
@@ -10,17 +10,55 @@ var con = mysql.createConnection({
     database: "mydb1"
 });
 
+// con.connect(function (err) {
+//     if (err) throw err;
+//     console.log("Connected!");
+//     var sql = "SELECT * FROM customers where address = 'Lahore'";
+//     con.query(sql, function (err, result, fields) {
+//         if (err) throw err;
+//         /** First show only result then show with loop and then show fields*/
+//         console.log(result);
+//         // console.log(fields);
+//         // result.forEach((item) => {
+//         //     console.log(`Name: ${item.name} | Address: ${item.address} | ID: ${item.id}`);
+//         // })
+//     });
+// });
+
+// Wildcards
+// con.connect(function (err) {
+//     if (err) throw err;
+//     console.log("Connected!");
+//     var sql = "SELECT * FROM customers WHERE address LIKE 'S%'";
+//     con.query(sql, function (err, result, fields) {
+//         if (err) throw err;
+//         console.log(result);
+//     });
+// });
+
+
+// if you want to pass it in the same line 
+// con.connect(function (err) {
+//     if (err) throw err;
+//     console.log("Connected!");
+//     var addr = "lahore"
+//     var sql = `SELECT * FROM customers WHERE address = ${mysql.escape(addr)}`;
+//     con.query(sql, function (err, result, fields) {
+//         if (err) throw err;
+//         console.log(result);
+//     });
+// });
+
+
+// with conditions
 con.connect(function (err) {
     if (err) throw err;
     console.log("Connected!");
-    var sql = "SELECT * FROM customers where address = 'Lahore'";
+    var addr = "lahore"
+    var sql = `SELECT * FROM customers WHERE address = 'Lahore' OR name = 'Amy'`;
     con.query(sql, function (err, result, fields) {
         if (err) throw err;
-        /** First show only result then show with loop and then show fields*/
         console.log(result);
-        // console.log(fields);
-        // result.forEach((item) => {
-        //     console.log(`Name: ${item.name} | Address: ${item.address} | ID: ${item.id}`);
-        // })
     });
 });
+
