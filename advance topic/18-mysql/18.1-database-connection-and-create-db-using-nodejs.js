2a876ae51f157f8f27f7d172a30f29599fadb272
Absar Ahmad <absarahmad.coder@gmail.com>
2022-03-25 11:54:35 +0500

    mysql in 18 folder

---
+++ advance topic/18-mysql/18.1-database-connection-and-create-db-using-nodejs.js
@@ -0,0 +1,18 @@
+// 18.1-database-connection-and-create-db-using-nodejs
+var mysql = require('mysql');
+var con = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "",
+});
+
+con.connect(function (err) {
+    if (err) throw err;
+    
+        con.query("CREATE DATABASE mydb1", function(err, result) {
+            if (err) throw err;
+            console.log("Database created");
+        })
+        console.log("Connected! to Server on http://localhost");
+    
+});
